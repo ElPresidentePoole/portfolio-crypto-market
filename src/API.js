2918a0f47c4f6f2e0ad2300300cb645cbe9536c5
Gordon Ivy Poole <gpoole@ooze.attlocal.net>
2023-05-04 05:56:27 -0400

    example code from coinmarketcap

--- src/API.js
+++ src/API.js
@@ -6,11 +6,36 @@ const myAxios = axios.create({
 });
 
 export default function testAxios() {
-  axios.get("/api/v1/cryptocurrency/listings/latest").then((response) => {
-    console.log(response);
-  }).catch((error) => {
-    console.log(error);
-  }).finally(() => {
-    console.log("Done with axios!");
-  });
+let response = null;
+new Promise(async (resolve, reject) => {
+  try {
+    response = await axios.get('/api/v1/cryptocurrency/listings/latest', {
+      headers: {
+        'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+      },
+    });
+  } catch(ex) {
+    response = null;
+    // error
+    console.log(ex);
+    reject(ex);
+  }
+  if (response) {
+    // success
+    const json = response.data;
+    console.log(json);
+    resolve(json);
+  }
+});
+
 }
+
+// export default function testAxios() {
+//   axios.get("/api/v1/cryptocurrency/listings/latest").then((response) => {
+//     console.log(response);
+//   }).catch((error) => {
+//     console.log(error);
+//   }).finally(() => {
+//     console.log("Done with axios!");
+//   });
+// }
