bc7d3db580f51d4f6ed303e7754d20d419fb6c1a
Gordon Ivy Poole <gpoole@ooze.attlocal.net>
2023-05-05 12:06:57 -0400

    ok for real im done for now

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -3,6 +3,7 @@
 export default function NavigationBar() {
   return (<nav className="navbar aria-label='page'">
             <div className="container-fluid justify-content-center">
+              <a className="navbar-brand" href="#">💰 real crypto site</a>
             (<a href="./index.html">Home</a>)
             (<a href="./prices.html">Latest Prices</a>)
             (<a href="./learn.html">Learn More</a>)
