2fe0813c070ffef90139444d753ecfc54c545482
Gordon Ivy Poole <gpoole@ooze.attlocal.net>
2023-05-05 12:04:06 -0400

    navbar bootstrapped

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,7 +1,11 @@
+
+// bg-dark class
 export default function NavigationBar() {
-  return (<nav>
+  return (<nav className="navbar aria-label='page'">
+            <div className="container-fluid justify-content-center">
             (<a href="./index.html">Home</a>)
             (<a href="./prices.html">Latest Prices</a>)
             (<a href="./learn.html">Learn More</a>)
+            </div>
           </nav>)
 }
